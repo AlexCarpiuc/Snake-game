15eb0002e6a6277dbf224ecaf96a2dec9f8d017a
Carpiuc Alex <alexcarpiuc@yahoo.com>
2025-03-13 11:05:47 +0200

    Add files via upload

---
+++ Elements/elements.py
@@ -0,0 +1,24 @@
+'''import pygame
+from pygame.math import Vector2
+import
+
+class Fruits:
+    def __init__(self):
+         Aceasta clasa trebuie sa creeze un obiect de tip rectangle, caruia ii voi
+        atribui o pozitie x si y, ce va fi atribuita random in timpul jocului
+        self.x=5
+        self.y=4
+        self_pos=Vector2(self.x,self.y)
+
+    def afisare(self):
+        fruit=pygame.Rect((self.pos.x,self.pos.y, patrat_dimensiune, patrat_dimensiune ))#
+
+
+
+
+
+
+
+
+class Snake:
+    pass'''
